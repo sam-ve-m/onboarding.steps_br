8c166994e9b1f5c50054f7beb4f56ca1afcd3c6b
Marco Sievers de Almeida <msa@lionx.com.br>
2022-10-17 17:26:37 -0300

    feat: merge

--- func/src/services/onboarding_steps/service.py
+++ func/src/services/onboarding_steps/service.py
@@ -28,12 +28,25 @@ class OnboardingSteps:
             unique_id=user_unique_id,
             bucket_name=cls.bucket_name,
         )
+        user_document_front_exists = cls.file_repository.user_file_exists(
+            file_type=UserDocument.DOCUMENT_FRONT,
+            unique_id=user_unique_id,
+            bucket_name=cls.bucket_name,
+        )
+        user_document_back_exists = cls.file_repository.user_file_exists(
+            file_type=UserDocument.DOCUMENT_BACK,
+            unique_id=user_unique_id,
+            bucket_name=cls.bucket_name,
+        )
+        user_document_exists = all(
+            await asyncio.gather(user_document_front_exists, user_document_back_exists)
+        )
 
         user = await cls.user_repository.find_user({"unique_id": user_unique_id})
         onboarding_step_builder = cls.steps_builder(user)
 
         onboarding_steps = await onboarding_step_builder.build(
-            selfie_exists=user_selfie_exists
+            selfie_exists=user_selfie_exists, document_exists=user_document_exists
         )
 
         return onboarding_steps
